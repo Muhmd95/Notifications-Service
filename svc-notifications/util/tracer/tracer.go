5cb3ad62ab19b2bbc8656e13d43bc34d802cd896
Mohamed Osama <mohamedossama513@gmail.com>
2026-08-16 19:15:07 +0300

    feat: Add logger and tracer utilities for OpenTelemetry integration

---
+++ svc-notifications/util/tracer/tracer.go
@@ -0,0 +1,24 @@
+package tracer
+
+import (
+	"go.opentelemetry.io/otel"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/propagation"
+)
+
+// intialize the tracer locally only without connecting to kibana
+// i will pass the service name to the func to know which service this tracer belongs
+func InitTracer(serviceName string) (*sdktrace.TracerProvider, error) {
+	tp := sdktrace.NewTracerProvider() // i will leave the exporter empty for now
+	// i will use kibana later
+
+	otel.SetTracerProvider(tp)
+	// this registers the global tracee so that when the otelhttp
+	// want to create a span it will use this tracer provider
+
+
+	// 2. CRITICAL: Set the global propagator so otelhttp knows how to 
+	// extract and inject Trace IDs into HTTP headers across your services.
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+	return tp, nil
+}
