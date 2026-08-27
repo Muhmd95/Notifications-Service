2bf263993f86ba4120962e8bb077e24a47a79094
Mohamed Osama <mohamedossama513@gmail.com>
2026-08-27 20:17:47 +0300

    Update TransactionEvent DTO structure
    Refactor the struct to match the incoming MongoDB document format,
    including updated field names and nested ID and timestamp structures.

--- svc-notifications/internal/notifications/dto.go
+++ svc-notifications/internal/notifications/dto.go
@@ -43,15 +43,39 @@ const (
 	EventWalletCredited = "WALLET_CREDITED"
 	EventWalletDebited  = "WALLET_DEBITED"
 )
+
+
+// {
+//   "_id": {"$oid": "67cf1a2b3c4d5e6f7a8b9c0d"},
+//   "reference_id": "dep-123",
+//   "phone_number": "01112223334",
+//   "sender_phone": "",
+//   "receiver_phone": "",
+//   "type": "DEPOSIT",
+//   "status": "POSTED",
+//   "amount": 500,
+//   "wallet_id": "67ae5f...",
+//   "balance_before": 0,
+//   "balance_after": 500,
+//   "sequence_number": 1,
+//   "created_at": {"$date": 1756300800000}
+// }
 type TransactionEvent struct {
-	EventType          string		`json:"event_type"`		 // "WALLET_CREDITED" | "WALLET_DEBITED"
-	TxnID              string		`json:"txn_id"`			 // transaction id
+	Type          string  			`json:"type"`		// "WALLET_CREDITED" | "WALLET_DEBITED" will need logic to be put
+	ID                 struct {
+		TxnID string `json:"$oid"`
+	}		`json:"_id"`			 // transaction id
+	SenderPhoneNumber   string		`json:"sender_phone"`	// when the transaction is transfer will be put with the sender	will need logic to be put
+	ReceiverPhoneNumber string		`json:"receiver_phone"`	// when the transaction is transfer will be put with the receiver	will need logic to be put
 	WalletID           string 		`json:"wallet_id"`			 // partition key
 	PhoneNumber        string		`json:"phone_number"`
 	NationalID         string 		`json:"national_id"`		// for the future will wire the user and their wallets
 	Amount             int64  		`json:"amount"`				// amount of the txn
 	BalanceAfter       int64  		`json:"balance_after"`
-	OccurredAt         time.Time 	`json:"occurred_at"`		// business time when the money moved
-	CoupledPhoneNumber string    	`json:"coupled_phone_number"` // when the transaction is transfer will be put with the sender						// must be checked first in the notifications service
+	OccurredAt         struct {
+		Date int64 `json:"$date"` // may be string watch for testing
+	} 	`json:"created_at"`		// business time when the money moved
+	CoupledPhoneNumber string    	 // when the transaction is transfer will be put with the sender	will need logic to be put					
+	// must be checked first in the notifications service
 }
 
